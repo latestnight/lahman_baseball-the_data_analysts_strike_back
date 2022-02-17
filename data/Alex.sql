696f4a9b779a4f73f398ee0a0c479a509c5e6e15
Alex <Alex77Fox@yahoo.com>
2022-02-17 14:45:07 -0500

    additional changes

--- data/Alex.sql
+++ data/Alex.sql
@@ -1,9 +1,11 @@
+-- 1
 -- What range of years for baseball games played does the provided database cover?
 -- Answer: 1871-2016
-SELECT MIN(year) AS starting_year,
-       MAX(year) AS ending_year
-FROM homegames;
+select min(year) as starting_year,
+       max(year) as ending_year
+from homegames;
 
+-- 2
 -- Find the name and height of the shortest player in the database.
 -- How many games did he play in? What is the name of the team for which he played?
 -- Answer: Edward Carl (43 inches), 1 game, St.Louis Browns
@@ -20,6 +22,7 @@ group by full_name, height, a.teamid, t.name, a.G_all, p.playerid
 order by height
 limit 1;
 
+-- 3
 -- Find all players in the database who played at Vanderbilt University.
 -- Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues.
 -- Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
@@ -38,6 +41,7 @@ group by cp, full_name, playerid, schoolid
 -- who knew?
 order by total_salary desc;
 
+-- 4
 -- Using the fielding table, group players into three groups based on their position:
 -- label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery".
 -- Determine the number of putouts made by each of these three groups in 2016.
@@ -54,6 +58,7 @@ from fielding
 where yearid = '2016'
 group by position_group;
 
+-- 5
 -- Find the average number of strikeouts per game by decade since 1920.
 -- Round the numbers you report to 2 decimal places. Do the same for home runs per game.
 -- Do you see any trends?
@@ -88,6 +93,7 @@ from decades
 group by decade
 order by decade;
 
+-- 6
 -- Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful.
 -- (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted at least 20 stolen bases.
 -- Answer: Chris Owings
@@ -110,20 +116,41 @@ select namefirst||' '||namelast as full_name,
 from sb_attempts join people using(playerid)
 order by sb_percentage desc;
 
+-- 7
 -- From 1970 – 2016, what is the largest number of wins for a team that did not win the world series?
 -- What is the smallest number of wins for a team that did win the world series?
 -- Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case.
 -- Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series?
 -- What percentage of the time?
-
 ronit
 
+--d
+with max_w_data as ( 
+                    select yearid,
+                           max(w) as max_w
+                    from teams
+                    where yearid between 1970 and 2016
+                    group by yearid
+                    order by yearid
+                   )
+select sum(case when wswin = 'Y' then 1 else 0 end) as maxw_wsw,
+--       count(wswin) as total_years,
+       concat(round(100*avg(case when wswin = 'Y' then 1 else 0 end), 2), '%') as maxw_wsw_pct
+--select max_w, wswin, teams.yearid
+from max_w_data
+join teams
+     on max_w_data.yearid = teams.yearid
+     and max_w_data.max_w = teams.w
+where wswin is not null;
+
+-- 8
 -- Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016
 -- (where average attendance is defined as total attendance divided by number of games).
 -- Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 
 kenny
 
+-- 9
 -- Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)?
 -- Give their full name and the teams that they were managing when they won the award.
 select namefirst || ' ' || namelast as full_name,
@@ -155,12 +182,14 @@ and awardsmanagers.playerid in
                               )
 order by full_name, year desc;
 
+-- 10
 -- Find all players who hit their career highest number of home runs in 2016.
 -- Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016.
 -- Report the players' first and last names and the number of home runs they hit in 2016.
 
 jeremy
 
+-- 11a
 -- Is there any correlation between number of wins and team salary? Use data from 2000 and later to answer this question.
 -- As you do this analysis, keep in mind that salaries across the whole league tend to increase together, so you may want to look on a year-by-year basis.
 with 
@@ -188,6 +217,7 @@ from rank_data
 group by salary_rank
 order by salary_rank;
 
+-- 11b
 -- Does there appear to be any correlation between attendance at home games and number of wins?
 with
 home_att_data as (
@@ -208,12 +238,13 @@ select aha_rank,
 from rank_data
 group by aha_rank
 order by aha_rank;
-                   
+
+-- 12
 -- Do teams that win the world series see a boost in attendance the following year? What about teams that made the playoffs?
 -- Making the playoffs means either being a division winner or a wild card winner.
 
 
-
+-- 13
 -- It is thought that since left-handed pitchers are more rare, causing batters to face them less often, that they are more effective.
 -- Investigate this claim and present evidence to either support or dispute this claim.
 -- First, determine just how rare left-handed pitchers are compared with right-handed pitchers. 
