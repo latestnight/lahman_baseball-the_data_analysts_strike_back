21b690700654dbbf457ddc9fd553e5f6d4ce676d
Alex <Alex77Fox@yahoo.com>
2022-02-18 12:28:04 -0500

    syntax edit

--- data/Alex.sql
+++ data/Alex.sql
@@ -67,7 +67,7 @@ group by position_group;
 -- 5
 -- Find the average number of strikeouts per game by decade since 1920.
 -- Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
-WITH decades AS (
+WITH decades as (
                      select yearid,
                             g,
                             so,
